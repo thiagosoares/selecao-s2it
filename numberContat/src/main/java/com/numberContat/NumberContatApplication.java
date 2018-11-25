079aca09dfd5143b7cc1cb712ab2147438ab4857
thiago soares <thiago.soares.jr@gmail.com>
2018-11-25 20:27:20 -0300

    Código- Questão 8: Inicio

---
+++ numberContat/src/main/java/com/numberContat/NumberContatApplication.java
@@ -0,0 +1,12 @@
+package com.numberContat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberContatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberContatApplication.class, args);
+	}
+}
---
+++ numberContat/src/test/java/com/numberContat/NumberContatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.numberContat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberContatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

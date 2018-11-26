a4939e980e7d6d3e1a398d0f98388002c498bae1
thiago soares <thiago.soares.jr@gmail.com>
2018-11-25 21:12:02 -0300

    Codigo - Questão 9: Inicio

---
+++ binaryTree/src/main/java/com/s2it/evaluation/binarytree/BinarytreeApplication.java
@@ -0,0 +1,12 @@
+package com.s2it.evaluation.binarytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarytreeApplication.class, args);
+	}
+}
